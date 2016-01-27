52e15de3de1f8df24fbbb4750789c920cfe67f1c
joksina <jksalaudeen@yahoo.com>
2016-01-26 19:36:22 -0800

    fickle change

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -2,8 +2,15 @@ var db = require("../db/db.js")
 
 module.exports = {
     signin : function(req, res){
-        //TODO: Write signin query
-
+     var user = req.body;
+     db.cypherQuery("MATCH (n:User) WHERE n.username={username} and n.password={password} RETURN n", user, function (err, result) {
+     	if (err) {
+     		throw error;
+     	} else  {
+     		console.log('i')
+     		res.send(result.data);
+     	}
+     })
     },
     signup: function(req, res){
       var userInfo = req.body
@@ -12,7 +19,7 @@ module.exports = {
               res.status(404).json(err);
             } else if (result.data.length!==0) {
               console.log(result)
-              res.status(400).send("Already a user. Please sign in.");
+              res.send("Already a user. Please sign in.");
             } else {
               db.cypherQuery("create (n:User {firstname:{firstname},lastname:{lastname},username:{username},email:{email},password:{password}}) return n", userInfo , function(err, result){
                     if(err) {
